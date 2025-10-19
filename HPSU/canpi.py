a2c8241277d0b54c40ee6795087272fb64441937
Christoph <petrus_1@gmx.de>
2025-10-19 14:22:37 +0200

    Adding the delay again to the loop using the timeout from conf file

--- HPSU/canpi.py
+++ HPSU/canpi.py
@@ -122,5 +122,7 @@ class CanPI(object):
                     self.hpsu.logger.error('CanPI %s, msg not sync, timeout' % cmd['name'])
                     notTimeout = False
                     rc = "KO"
+                else:
+                    time.sleep(self.timeout + 0.05)
 
         return rc
